e6e7b7f7fd19081135e52611dfbf3ffe76ef7807
galatsu <tracy4fun.minecraft@gmail.com>
2022-09-25 01:49:56 -0400

    second update
    headed out for the night

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
+    public float speed;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +14,25 @@ public class CameraFollow : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 currentPosition = transform.position;
+
+        if (Input.GetKey("d"))
+        {
+            currentPosition.x += speed;
+        }
+        if (Input.GetKey("a"))
+        {
+            currentPosition.x -= speed;
+        }
+        if (Input.GetKey("w"))
+        {
+            currentPosition.y += speed;
+        }
+        if (Input.GetKey("s"))
+        {
+            currentPosition.y -= speed;
+        }
+
+        transform.position = currentPosition;
     }
 }
